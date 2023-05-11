7cc90ba9b6433ace4f0a66f476161aad231b249d
Anna <annajaninaschmidt@gmail.com>
2023-05-11 12:39:24 +1000

    about me updated

--- src/components/About.js
+++ src/components/About.js
@@ -1,19 +1,47 @@
 import React from "react";
 import img1 from "../images/Anna.jpeg";
 
-function AboutMe() {
-    return (
-        <section className="container mx-auto">
-            <div className="flex justify-between items-center py-10">
-                <img className="w-44 h-44" src={img1} alt="Anna profile" />
-                <p>Hello, my name is Anna. I switched my career from being a Dental Hygienist and Makeup Artist to become a Web Developer. </p>
-            </div>
+// function AboutMe() {
+//     return (
+//         <section className="container mx-auto">
+//             <div className="flex justify-between items-center py-10">
+//                 <img className="w-44 h-44" src={img1} alt="Anna profile" />
+//                 <p>I am a multifaceted individual with a diverse background and many life experiences. Originally from Germany, I started my career as a Dental Hygienist before moving to Australia where I transitioned into the world of Makeup Artistry and Teaching. 
+//                     <br></br>
+//                 Now, I am pursuing my passion for technology by completing a Full Stack Coding Bootcamp at Monash University. Through this program, I have gained proficiency in various programming languages and tools such as JavaScript, React, HTML, CSS, Bootstrap, Tailwind, Visual Studio Code, SQL, Heroku, Git, MySQL, MongoDB, Node.js, Express, and MERN Stack. With my strong communication skills, ability to work well under pressure, and attention to detail, I am excited to bring my unique perspective and skill set to my next career opportunity. </p>
+//             </div>
           
 
 
 
+//         </section>
+//     )
+// };
+
+
+function AboutMe() {
+    return (
+        <section className="container mx-auto">
+            <div className="flex justify-center items-center py-16">
+                <img className="w-64 h-64 rounded-full mr-10" src={img1} alt="Anna profile" />
+                <div className="px-8">
+                    <p className="text-center md:text-left">I am a multifaceted individual with a diverse background and many life experiences. Originally from Germany, I started my career as a Dental Hygienist before moving to Australia where I transitioned into the world of Makeup Artistry and Teaching. 
+                    <br></br>
+                Now, I am pursuing my passion for technology by completing a Full Stack Coding Bootcamp at Monash University. Through this program, I have gained proficiency in various programming languages and tools such as JavaScript, React, HTML, CSS, Bootstrap, Tailwind, Visual Studio Code, SQL, Heroku, Git, MySQL, MongoDB, Node.js, Express, and MERN Stack. With my strong communication skills, ability to work well under pressure, and attention to detail, I am excited to bring my unique perspective and skill set to my next career opportunity. </p>
+                </div>
+            </div>
         </section>
     )
 };
+
+
+
+
+
+
+
+
+
+
 
 export default AboutMe;
